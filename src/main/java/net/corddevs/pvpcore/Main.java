ea164f192e36cf9fc941a6f664cc4a82fb971518
DaatUserName <40370496+DaatUserName@users.noreply.github.com>
2020-12-29 22:11:22 +0100

    Update Main.java

--- src/main/java/net/corddevs/pvpcore/Main.java
+++ src/main/java/net/corddevs/pvpcore/Main.java
@@ -6,6 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
 
+    public static Main instance;
+
     @Override
     public void onEnable() {
         if (Bukkit.getPluginManager().isPluginEnabled("PlaceHolderAPI")) {
@@ -41,5 +43,9 @@ public final class Main extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Main getInstance() {
+        return Main.instance;
     }
 }
